7722a648e2785701dda0e3af0218ed423522039f
Angela Guerra <118551747+JAMIXELL@users.noreply.github.com>
2023-04-24 15:09:04 -0500

    main

---
+++ scripts/operador.js
@@ -0,0 +1,8 @@
+//Conversion larga 
+var celsius = prompt ("Introduzca la temperatura en grados celsius");
+celsius = parseFloat (celsius);
+
+// conversion corta
+// var celsius = parseFloat (prompt ("Introduzca la temperatura en grados cdelsius"));
+var f= 9/5 *celsius+32;
+document.write ("La conversion es:", f);
